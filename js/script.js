f58a7f2cbd6333cc166c33876347d95cc23a500e
José Marcel <josemarcel30@gmail.com>
2023-10-20 12:39:14 -0300

    Criando a cobrinha com java scrpit parte 2

--- js/script.js
+++ js/script.js
@@ -1,13 +1,43 @@
+
 const canvas = document.getElementById("meuCanvas")
 const ctx =  canvas.getContext("2d")
 
+
+
+// tamanho da cobrinha
 const size = 30
 
-const snake = [{x:200, y:200}]
+// posição da cobrinha com array
+const snake = [
+    {x:200, y:200},
+    {x:230, y:200}
+]
 
+// fazer a cobra se mexer
+ let direction  
+
+// função para criar a cobrinha 
 const drawSnake = () =>{
+// cor da corinha 
     ctx.fillStyle = "#ddd"
-    ctx.fillRect(snake[0].x, snake[0].y, size, size)
+// função para definir a posção 
+   snake.forEach((position, index) => {
+// definir a cor da cabeça da cobra
+    if(index == snake.length -1){
+        ctx.fillStyle = "blue"
+         
+
+    }
+// criando a cobra com as posição de x e y e com size do tamanho 30
+    ctx.fillRect(position.x, position.y, size,  size)
+   })
 }
 
+// mover a cobrinha
+
+const moveSnaker = () => {
+    
+}
+
+// função da cobra 
 drawSnake()
