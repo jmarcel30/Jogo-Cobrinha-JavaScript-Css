99de0206afc45eae25cf4efd047da4b9fdc1c53f
José Marcel <josemarcel30@gmail.com>
2023-10-20 12:01:51 -0300

    Criando a cobrinha com java scrpit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+const canvas = document.getElementById("meuCanvas")
+const ctx =  canvas.getContext("2d")
+
+const size = 30
+
+const snake = [{x:200, y:200}]
+
+const drawSnake = () =>{
+    ctx.fillStyle = "#ddd"
+    ctx.fillRect(snake[0].x, snake[0].y, size, size)
+}
+
+drawSnake()
